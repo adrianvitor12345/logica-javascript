50e776e96120ce7a9310943e9cd0e676d6d14ec0
adrianvitor12345 <165900572+adrianvitor12345@users.noreply.github.com>
2024-08-28 07:59:16 -0300

    000123

---
+++ index.js
@@ -0,0 +1,16 @@
+var nome = "adrian vitor"; 
+let idade = 16 ;
+const pi= 3.14;
+
+let a =5; 
+let b =10;
+let soma = a+b;
+
+let a= 5;
+let b=10;
+
+function multiplica(x, y)
+
+let produto= multiplica (a, b)
+
+let frutaad= ['apple', 'banana', 'cherry'];
